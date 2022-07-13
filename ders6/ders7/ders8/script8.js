1e51dff820b2e56456e76dbb74cd925e158c7f5d
kayra <kayrabeyy54@gmail.com>
2022-07-13 23:29:46 +0300

    aritmetik operatör

--- ders6/ders7/ders8/script8.js
+++ ders6/ders7/ders8/script8.js
@@ -1,3 +1,29 @@
-var isim = "emir"; // string
-var yaş = 21; // number
-var eğitimdurumu = true; //bool
+var birincisayı = "5";
+var ikincisayı = "8";
+
+var toplam = birincisayı + ikincisayı;
+document.write(toplam);
+document.write("<br/>");
+
+
+
+
+var çarpım = birincisayı * ikincisayı;
+document.write(çarpım);
+document.write("<br/>");
+
+
+
+var fark = ikincisayı - birincisayı;
+document.write(fark);
+document.write("<br/>");
+
+
+var bölüm = ikincisayı / birincisayı;
+document.write(bölüm);
+document.write("<br/>");
+
+
+var mod = ikincisayı % birincisayı; // modülüs
+document.write(mod);
+document.write("<br/>");
