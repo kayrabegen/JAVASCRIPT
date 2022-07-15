79b50f673f0a41f7b74f8c0751fb1a87a9ae821f
kayra <kayrabeyy54@gmail.com>
2022-07-15 16:35:04 +0300

    Atama operatörleri

---
+++ script12.js
@@ -0,0 +1,27 @@
+var sayı = 5; // = atama işlemini yapar
+
+
+
+// x == y  x ile y eşit mi diye kontrol eder
+
+document.write(sayı);
+document.write("<br/>");
+
+sayı += 5;
+
+document.write(sayı);
+document.write("<br/>");
+
+sayı -= 3;
+
+document.write(sayı);
+document.write("<br/>");
+
+sayı *= 5;
+
+document.write(sayı);
+document.write("<br/>");
+
+sayı /= 7;
+document.write(sayı);
+document.write("<br/>");
