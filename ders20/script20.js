fa9ea7f8efbe5f2e14c83340d92431ca0e3561df
kayra <kayrabeyy54@gmail.com>
2022-07-18 23:58:49 +0300

    slice metodu

---
+++ ders20/script20.js
@@ -0,0 +1,8 @@
+var text = "javascipt proglamlama dili";
+
+var new_text = text.slice(10); //0 dan 10 kadar keser , kalan kısmı yeni değişkene atar
+document.write(new_text);
+
+
+var new_text2 = text.slice(5, 10);
+document.write(new_text2);
