1d2d82a543730554b2700f92d739bec87339e22b
kayra <kayrabeyy54@gmail.com>
2022-07-19 17:34:26 +0300

    Substring metodunun kullanımı

---
+++ ders21/script21.js
@@ -0,0 +1,5 @@
+var text = "Hello world"; // index yani sayma sayısı 0 dan baslar (Javascript dilinde)
+
+var new_text = text.substring(0, 5); //5 e kadar =>0 1 2 3 4  substring son değeri dahil etmez
+
+document.write(new_text);
