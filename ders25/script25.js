b4df4dee6762b7b4f57155d37ab660ddbd701c1c
kayra <kayrabeyy54@gmail.com>
2022-07-20 00:01:37 +0300

    Number metodunun kullanımı

---
+++ ders25/script25.js
@@ -0,0 +1,31 @@
+var value1 = "6";
+var value2 = true;
+var value3 = false;
+
+
+
+document.writeln(typeof (value1));
+document.writeln(typeof (value2));
+document.writeln(typeof (value3));
+
+
+document.writeln("<br/>");
+
+var newValue1 = Number(value1);
+var newValue2 = Number(value2);
+var newValue3 = Number(value3);
+
+
+document.writeln(typeof (newValue1));
+document.writeln(typeof (newValue2));
+document.writeln(typeof (newValue3));
+
+
+document.writeln("<br/>");
+
+document.writeln(newValue1);
+document.writeln(newValue2);
+document.writeln(newValue3);
+
+
+// binary nedir ?
